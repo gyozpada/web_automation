df75a9dc921972cbb3e2b89a3697b3b62a87f56d
BLANKONTECH <blankontech@BLANKONTECHs-MacBook-Air.local>
2023-01-06 17:35:37 +0700

    update test google search

--- SampleTest.js
+++ SampleTest.js
@@ -1,12 +1,16 @@
-const { Builder } = require('selenium-webdriver');
+const { By, Builder, Key } = require('selenium-webdriver');
+require("chromedriver");
 
-(async function example(){
-    let driver = new Builder().forBrowser('chrome').build();
+async function example(){
+    let driver = await new Builder().forBrowser('chrome').build();
 
-    try{
         await driver.get("https:/www.google.com");
-    }
-    finally{
+        await driver.findElement(By.name("q")).sendKeys("This text will be texted in Search Bar", Key.RETURN);
         await driver.quit();
-    }
-})();
+
+        setInterval(function(){
+            driver.quit();
+        }, 10000);
+};
+
+example();
