a75a170297528fdbcc27b2ca6b01a3b30bcae53f
galang <galanglost@gmail.com>
2023-01-09 18:43:39 +0700

    update js

--- SampleTest2.js
+++ SampleTest2.js
@@ -1,4 +1,4 @@
-const {By, Key, Builder} = require("selenium-webdriver");
+const {By, Key, Builder, Browser} = require("selenium-webdriver");
 require("chromedriver");
 
 async function test_case(){
@@ -17,24 +17,45 @@ async function test_case(){
 
     //Test#1
     //if the tittle isn't correct,it will failed
-    if(await driver.getTitle() === "GitHub: Let’s build from here · GitHub"){
-        console.log("Test #1 \x1b[32mPass")
-    } else {
-        console.log("Test #1 \x1b[31Failed");
-    }
+    await driver.getTitle() === "GitHub: Let’s build from here · GitHub" ? PrintResult(true) : PrintResult(false)
+    // if(await driver.getTitle() === "GitHub: Let’s build from here · GitHub"){
+    //     console.log("Test #1 \x1b[32mPass....")
+    // } else {
+    //     console.log("Test #1 \x1b[31Failed....");
+    // }
+
+    //set time await to load page
+    await driver.manage().setTimeouts( {implicit: 10000} );
 
     //input a username and passwordlet a1 = driver.actions();
-    a1.pause(100).perform();
     await driver.findElement(By.name("login")).sendKeys("ThisTextInUsernameBox");
     await driver.findElement(By.name("password")).sendKeys("**********", Key.RETURN);
 
+    function PrintResult(boolean){
+        boolean ? console.log(" \x1b[32mPass") : console.log(" \x1b[31Failed")
+    }
+
+    async function ClassNameTest(cssClassName){
+     const result =  await driver.findElement(By.className(cssClassName)).isDisplayed() ? PrintResult(true) : PrintResult(false)
+     return result
+    }
+
+    async function TextCheck(ElementText){
+        const result =  await driver.findElement(By.className(ElementText)).isDisplayed() ? PrintResult(true) : PrintResult(false)
+        return result
+    }
+
+
     //Test#2
     //if the corrected elemet isn't displayed, it will failed
-    if(await driver.findElement(By.className("flash-close js-flash-close")).isDisplayed()){
-        console.log("Test #2 Success");
-    } else {
-        console.log("Test #2 Failed");
-    }
+    // if(await driver.findElement(By.className("flash-close js-flash-close")).isDisplayed()){
+    //     console.log("Test #2 Success");
+    // } else {
+    //     console.log("Test #2 Failed");
+    // }
+
+    ClassNameTest("flash flash-full flash-error")
+    // TextCheck()
 
     setInterval(function(){
         driver.quit();
