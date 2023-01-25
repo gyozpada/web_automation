ff769195c6ab4cee9330acd602dfa776ea1a8e16
galang <galanglost@gmail.com>
2023-01-25 19:33:55 +0700

    update comment

--- upload.js
+++ upload.js
@@ -2,12 +2,15 @@ const {By, Key, Builder} = require("selenium-webdriver");
 require("chromedriver");
 
 async function upload(){
+
+    //create driver
     let driver = await new Builder().forBrowser("chrome").build();
 
     await driver.get('https://filebin.net/');
 
     let input = await driver.findElement(By.id("fileField"));
 
+    //form.click();
     await input.sendKeys("C:\\Users\\jrobi\\OneDrive\\Desktop");
 
     await form.click();
